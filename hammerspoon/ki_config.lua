4643d2ec2435b714c8c5422e0306a593cc081f1a
jokajak <jokajak@gmail.com>
2019-08-21 19:48:57 -0400

    update ki config with new shortcuts

--- hammerspoon/ki_config.lua
+++ hammerspoon/ki_config.lua
@@ -4,11 +4,6 @@ local function openURLEvent(url)
     end
 end
 
-local function send_keystroke(_keystroke)
-    return hs.eventtap.keyStroke({}, _keystroke)
-end
-
-local prevFrameSizes = {}
 local _lunette = spoon.Lunette
 
 local function moveAndResize(option, exit_mode)
@@ -33,10 +28,10 @@ Install:andUse('Ki', {
         local Firefox = Application:new("Firefox")
         local VSCode = Application:new("Visual Studio Code")
         local iTerm = Application:new("iTerm2")
+        local Stickies = Application:new("Stickies")
+        local XMind = Application:new("XMind")
         -- Define normal mode workflows
         local normalEvents = {
-            { nil, "j", send_keystroke("Down"), {"Normal Mode", "↓"}},
-            { nil, "k", send_keystroke("Up"), {"Normal Mode", "↑"}}
         }
         -- Define custom entity workflows
         local entityEvents = {
@@ -44,6 +39,8 @@ Install:andUse('Ki', {
             { nil, "f", Firefox, { "Entities", "Firefox" }},
             { nil, "v", VSCode, { "Entities", "Code" }},
             { nil, "i", iTerm, { "Entities", "iTerm2" }},
+            { nil, "s", Stickies, { "Entities", "Stickies" }},
+            { nil, "x", XMind, { "Entities", "XMind" }},
         }
         -- Use relevant application entities that can have multiple windows to be available in select mode
         local selectEntityEvents = {
@@ -59,11 +56,12 @@ Install:andUse('Ki', {
         }
         local actionEvents = {
             { nil, "c", function() moveAndResize("center", true) end, { "Action Events", "Center"} },
-            { nil, "f", function() moveAndResize("fullscreen", true) end, { "Action Events", "Fullscreen"} },
+            { nil, "f", function() moveAndResize("fullScreen", true) end, { "Action Events", "Fullscreen"} },
             { nil, "m", function() moveAndResize("maximize", true) end, { "Action Events", "Maximize"} },
-            { nil, "s", function() moveAndResize("shrink") end, { "Action Events", "Shrink"} },
-            { nil, "u", function() moveAndResize("undo") end, { "Action Events", "Undo"} },
-            { nil, "left", function() moveAndResize("left") end, { "Action Events", "Shrink"} },
+            { nil, "z", function() moveAndResize("undo") end, { "Action Events", "Undo"} },
+            { {"shift"}, "z", function() moveAndResize("redo") end, { "Action Events", "Undo"} },
+            { nil, "down", function() moveAndResize("shrink") end, { "Action Events", "Shrink"} },
+            { nil, "up", function() moveAndResize("expand") end, { "Action Events", "Expand"} },
             { {'shift'}, "r", function() hs.reload() end, { "Action Events", "Reload Hammerspoon"} },
         }
 
