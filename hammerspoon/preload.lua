1845df832994e86373d1f7bed5aef076ef00b7b9
jokajak <jokajak@gmail.com>
2019-08-24 07:42:36 -0400

    Add preload.lua from 1.
    1: https://github.com/jeoygin/awesome-hammerspoon/blob/master/preload.lua

---
+++ hammerspoon/preload.lua
@@ -0,0 +1,33 @@
+-- The default duration for animations, in seconds. Initial value is 0.2; set to 0 to disable animations.
+hs.window.animationDuration = 0
+
+-- auto reload config
+configFileWatcher =
+  hs.pathwatcher.new(hs.configdir, function(files)
+      local isLuaFileChange = utils.some(files, function(p)
+          return not (string.match(p, "^.+/([^%.#][^/]+%.lua)$") == nil)
+      end)
+      if isLuaFileChange then
+          hs.reload()
+      end
+  end):start()
+
+-- persist console history across launches
+hs.shutdownCallback = function() hs.settings.set('history', hs.console.getHistory()) end
+hs.console.setHistory(hs.settings.get('history'))
+
+-- ensure CLI installed
+hs.ipc.cliInstall()
+
+-- helpful aliases
+i = hs.inspect
+fw = hs.window.focusedWindow
+fmt = string.format
+bind = hs.hotkey.bind
+alert = hs.alert.show
+clear = hs.console.clearConsole
+reload = hs.reload
+pbcopy = hs.pasteboard.setContents
+std = hs.stdlib and require("hs.stdlib")
+utils = hs.fnutils
+hyper = {'⌘', '⌃'}
