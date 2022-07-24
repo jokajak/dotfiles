c64d2b5db7b17d2d0d5ebef37fa0fc1a0e1ef0a5
Josh <jokajak@gmail.com>
2022-07-24 18:59:45 -0400

    neovim: add lsp and nvim completions

--- neovim/.config/nvim/lua/jokajak/plugins/init.lua
+++ neovim/.config/nvim/lua/jokajak/plugins/init.lua
@@ -54,6 +54,8 @@ packer.startup(function(use)
   use "hrsh7th/cmp-path" -- path completions
   use "hrsh7th/cmp-cmdline" -- cmdline completions
   use "saadparwaiz1/cmp_luasnip" -- snippet completions
+  use "hrsh7th/cmp-nvim-lsp"  -- lsp completions
+  use "hrsh7th/cmp-nvim-lua"  -- nvim lua completions
 
   -- snippets
   use "L3MON4D3/LuaSnip" --snippet engine
