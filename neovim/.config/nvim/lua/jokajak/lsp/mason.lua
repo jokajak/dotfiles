0d5eed20792e4821f13b6e45733f3aa020bef7b9
Josh <jokajak@gmail.com>
2022-07-24 17:47:28 -0400

    neovim: get lsp support for lua working

--- neovim/.config/nvim/lua/jokajak/lsp/mason.lua
+++ neovim/.config/nvim/lua/jokajak/lsp/mason.lua
@@ -14,19 +14,15 @@ mason.setup({
   }
 })
 
-local status_ok, lspconfig = pcall(require, "mason-lspconfig")
+local status_ok, mason_lspconfig = pcall(require, "mason-lspconfig")
+local lspconfig = require("lspconfig")
+local handlers = require("jokajak.lsp.handlers")
 
 if not status_ok then
   return
 end
 
-local status_ok, _ = pcall(require, "lspconfig")
-
-if not status_ok then
-  return
-end
-
-lspconfig.setup({
+mason_lspconfig.setup({
     ensure_installed = {
       "sumneko_lua",  -- lua
       "python-lsp-server",  -- python support
@@ -34,26 +30,28 @@ lspconfig.setup({
     }
 })
 
-lspconfig.setup_handlers({
+mason_lspconfig.setup_handlers({
   -- The first entry (without a key) will be the default handler
   -- and will be called for each installed server that doesn't have
   -- a dedicated handler.
   function (server_name) -- default handler (optional)
-    require("lspconfig")[server_name].setup {}
+    local opts = {
+      on_attach = handlers.on_attach,
+      capabilities = handlers.capabilities
+    }
+    lspconfig[server_name].setup(opts)
   end,
   -- Next, provide targeted overrides for specific servers.
-  ["sumneko_lua"] = function ()
-    local lsp = lspconfig[lsp]
+  ["sumneko_lua"] = function(server_name)
+    local lsp = lspconfig[server_name]
+    local opts = {
+      on_attach = handlers.on_attach,
+      capabilities = handlers.capabilities
+    }
+    local sumneko_opts = require("jokajak.lsp.settings.sumneko_lua")
+	 	opts = vim.tbl_deep_extend("force", sumneko_opts, opts)
     if lsp then
-      lsp.setup({
-        settings = {
-          Lua = {
-            diagnostics = {
-              globals = { "vim" }
-            }
-          }
-        }
-      })
+      lsp.setup(opts)
     end
   end,
 })
