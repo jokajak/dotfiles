06f2b7971e44c97e10d06a0a15ea6036c68a11a1
jokajak <jokajak@gmail.com>
2022-07-26 12:18:41 -0400

    neovim: configure new plugins
    * Setup alpha based on nvim from scratch
    * Configure gitlinker for easy links
    * Set up impatient to speed up starting
    * Set up indentline
    * Configure twilight

---
+++ neovim/.config/nvim/lua/jokajak/plugins/impatient.lua
@@ -0,0 +1,8 @@
+--[[ impatient config ]]--
+local status_ok, impatient = pcall(require, "impatient")
+
+if not status_ok then
+  return
+end
+
+impatient.enable_profile()
---
+++ neovim/.config/nvim/lua/jokajak/plugins/indentline.lua
@@ -0,0 +1,35 @@
+--[[ indentline configuration ]]--
+local status_ok, indentline = pcall(require, "indent_blankline")
+
+if not status_ok then
+  return
+end
+
+vim.cmd [[highlight IndentBlanklineIndent1 guifg=#E06C75 gui=nocombine]]
+vim.cmd [[highlight IndentBlanklineIndent2 guifg=#E5C07B gui=nocombine]]
+vim.cmd [[highlight IndentBlanklineIndent3 guifg=#98C379 gui=nocombine]]
+vim.cmd [[highlight IndentBlanklineIndent4 guifg=#56B6C2 gui=nocombine]]
+vim.cmd [[highlight IndentBlanklineIndent5 guifg=#61AFEF gui=nocombine]]
+vim.cmd [[highlight IndentBlanklineIndent6 guifg=#C678DD gui=nocombine]]
+
+vim.opt.list = true
+vim.opt.listchars:append("space:⋅")
+vim.opt.listchars:append("eol:↴")
+-- use a character with no gaps on the left
+vim.g.indent_blankline_char = "▏"
+
+local setup = {
+  space_char_blankline = " ",
+  show_current_context = true,  -- highlight current indentation based on treesitter
+  show_current_context_start = true,
+  char_highlight_list = {
+    "IndentBlanklineIndent1",
+    "IndentBlanklineIndent2",
+    "IndentBlanklineIndent3",
+    "IndentBlanklineIndent4",
+    "IndentBlanklineIndent5",
+    "IndentBlanklineIndent6",
+  },
+}
+
+indentline.setup(setup)
