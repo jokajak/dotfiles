d7489503e702e27aa428da2300476226bbaaba2a
jokajak <jokajak@gmail.com>
2022-08-24 16:40:54 -0400

    nvim: add zp to preview folds
    disables fold-preview mappings

--- neovim/.config/nvim/lua/jokajak/plugins/fold-preview.lua
+++ neovim/.config/nvim/lua/jokajak/plugins/fold-preview.lua
@@ -5,4 +5,22 @@ if not status_ok then
   return
 end
 
-fpreview.setup()
+fpreview.setup({
+  default_keybindings = false
+})
+
+-- fold preview keymap-amend wasn't working
+-- use my own remaps
+
+local fn = vim.fn
+
+-- Copy/paste of 
+local preview_fold = function()
+  local current_line_folded = fn.foldclosed('.') ~= -1
+  if current_line_folded then
+    fpreview.show_preview()
+  else
+
+  end
+end
+vim.keymap.set("n", "zp", preview_fold, { silent = true, noremap= true, desc = "Preview fold" })
