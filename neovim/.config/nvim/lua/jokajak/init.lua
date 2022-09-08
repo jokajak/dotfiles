056c04a7077a71cb4e29456285f7b78507f20685
Josh <jokajak@gmail.com>
2022-09-07 23:09:51 -0400

    nvim: get rid of dumb lazy load approach

--- neovim/.config/nvim/lua/jokajak/init.lua
+++ neovim/.config/nvim/lua/jokajak/init.lua
@@ -9,5 +9,3 @@ require('jokajak.colorscheme') -- colorscheme
 require('jokajak.plugins')      -- Plugins`
 require('jokajak.lsp')       -- Language Server Protocol
 require('jokajak.autocmds')   -- auto commands
-local lazy_loader = require('jokajak.lazy_load')  -- lazy load
-lazy_loader.git()
