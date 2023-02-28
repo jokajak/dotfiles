9fbe6af0769e5980631dd73c87b2b58d338737b2
Josh <jokajak@gmail.com>
2023-02-28 08:27:11 -0500

    neovim: add tsnode-marker and mini-bracketed

---
+++ neovim/.config/nvim/lua/plugins/mini-bracketed.lua
@@ -0,0 +1,6 @@
+-- https://github.com/echasnovski/mini.bracketed/
+-- Go forward/backward with square brackets
+
+local M = { "echasnovski/mini.bracketed", version = false }
+
+return M
---
+++ neovim/.config/nvim/lua/plugins/tsnode-marker.lua
@@ -0,0 +1,21 @@
+-- https://github.com/atusy/tsnode-marker.nvim
+-- Mark treesitter node to enhance context changes in your buffer.
+-- E.g. use different background for code blocks in markdown
+local M = {
+  "atusy/tsnode-marker.nvim",
+  lazy = true,
+  init = function()
+    vim.api.nvim_create_autocmd("FileType", {
+      group = vim.api.nvim_create_augroup("tsnode-marker-markdown", {}),
+      pattern = "markdown",
+      callback = function(ctx)
+        require("tsnode-marker").set_automark(ctx.buf, {
+          target = { "code_fence_content" }, -- list of target node types
+          hl_group = "CursorLine", -- highlight group
+        })
+      end,
+    })
+  end,
+}
+
+return M
