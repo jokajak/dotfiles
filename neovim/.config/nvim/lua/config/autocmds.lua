6b855f2f026d7b870841d1017a7df7c629da9229
Josh <jokajak@gmail.com>
2023-03-05 21:37:10 -0500

    neovim: configure mini-bracketed

--- neovim/.config/nvim/lua/config/autocmds.lua
+++ neovim/.config/nvim/lua/config/autocmds.lua
@@ -29,17 +29,6 @@ autocmd({ "BufLeave", "FocusLost", "InsertEnter", "WinLeave" }, {
   end,
 })
 
--- -- [[ Highlight on yank ]]
--- -- See `:help vim.highlight.on_yank()`
--- local highlight_group = vim.api.nvim_create_augroup("YankHighlight", { clear = true })
--- vim.api.nvim_create_autocmd("TextYankPost", {
---   callback = function()
---     vim.highlight.on_yank()
---   end,
---   group = highlight_group,
---   pattern = "*",
--- })
-
 -- trim trailing whitespace
 
 local trim_patterns = {
--- neovim/.config/nvim/lua/plugins/mini-bracketed.lua
+++ neovim/.config/nvim/lua/plugins/mini-bracketed.lua
@@ -1,6 +1,13 @@
 -- https://github.com/echasnovski/mini.bracketed/
 -- Go forward/backward with square brackets
 
-local M = { "echasnovski/mini.bracketed", version = false }
+local M = {
+  "echasnovski/mini.bracketed",
+  version = false,
+  event = "VeryLazy",
+  config = function(_, opts)
+    require("mini.bracketed").setup(opts)
+  end,
+}
 
 return M
