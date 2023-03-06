1f7f0b41f15990c041dfe5c1f1ca5e6cd357306a
jokajak <jokajak@gmail.com>
2023-03-06 17:14:43 -0500

    wezterm: set colorscheme based on time

--- wezterm/.config/wezterm/colors.lua
+++ wezterm/.config/wezterm/colors.lua
@@ -1,5 +1,12 @@
 local M = {}
 
-M.color_scheme = "tokyonight-day"
+-- set background based on time
+local now = os.date("*t")
+
+if (6 <= now.hour) and (now.hour < 20) then
+	M.color_scheme = "tokyonight-day"
+else
+	M.color_scheme = "tokyonight-storm"
+end
 
 return M
