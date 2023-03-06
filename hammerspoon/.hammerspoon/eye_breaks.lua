22a27b43bc599bbb79a035c5a9d309cdd6f45353
jokajak <jokajak@gmail.com>
2023-03-06 17:14:27 -0500

    hammerspoon: play a sound during eye breaks

--- hammerspoon/.hammerspoon/eye_breaks.lua
+++ hammerspoon/.hammerspoon/eye_breaks.lua
@@ -3,54 +3,63 @@ local module = {}
 
 local drawing = require("hs.drawing")
 local timer = require("hs.timer")
-local uuid    = require"hs.host".uuid
+local uuid = require("hs.host").uuid
 local _duration = 20
 
 module._screenShades = {}
+module._start_sound_name = "Morse"
+module._stop_sound_name = "Blow"
+
+local start_sound = require("hs.sound").getByName(module._start_sound_name)
+local stop_sound = require("hs.sound").getByName(module._stop_sound_name)
 
 local purgeShade = function(UUID)
-    local indexToRemove
-    for i,v in ipairs(module._screenShades) do
-        if v.UUID == UUID then
-            if v.timer then v.timer:stop() end
-            v.drawing:hide()
-            indexToRemove = i
-            break
-        end
-    end
-    if indexToRemove then
-        table.remove(module._screenShades, indexToRemove)
-    end
+	local indexToRemove
+	for i, v in ipairs(module._screenShades) do
+		if v.UUID == UUID then
+			if v.timer then
+				v.timer:stop()
+			end
+			v.drawing:hide()
+			indexToRemove = i
+			break
+		end
+	end
+	if indexToRemove then
+		table.remove(module._screenShades, indexToRemove)
+	end
+	stop_sound:play()
 end
 
 function module:shade()
-    hs.alert.closeAll()
-    local str = "Look 20 feet away for 20 seconds."
-    hs.fnutils.imap(hs.screen.allScreens(), function(screen)
-        local UUID = uuid()
-        local shade = {
-            drawing = drawing.rectangle(screen:fullFrame()),
-            screen = screen,
-            UUID = UUID
-        }
+	hs.alert.closeAll()
+	local str = "Look 20 feet away for 20 seconds."
+	hs.fnutils.imap(hs.screen.allScreens(), function(screen)
+		local UUID = uuid()
+		local shade = {
+			drawing = drawing.rectangle(screen:fullFrame()),
+			screen = screen,
+			UUID = UUID,
+		}
 
-        --shade characteristics
-        --white - the ratio of white to black from 0.0 (completely black) to 1.0 (completely white); default = 0.
-        --alpha - the color transparency from 0.0 (completely transparent) to 1.0 (completely opaque)
-        shade.drawing:setFillColor({["white"] = 0, ["alpha"] = 0.7})
+		--shade characteristics
+		--white - the ratio of white to black from 0.0 (completely black) to 1.0 (completely white); default = 0.
+		--alpha - the color transparency from 0.0 (completely transparent) to 1.0 (completely opaque)
+		shade.drawing:setFillColor({ ["white"] = 0, ["alpha"] = 0.8 })
 
-        --set to cover the whole screen, all spaces and expose
-        shade.drawing:bringToFront(true):setBehavior(17)
+		--set to cover the whole screen, all spaces and expose
+		shade.drawing:bringToFront(true):setBehavior(17)
 
-        shade.drawing:show()
-        table.insert(module._screenShades, shade)
-        shade.timer = timer.doAfter(_duration, function()
-            purgeShade(UUID)
-        end)
-        return hs.alert.show(str, hs.alert.defaultStyle, screen, _duration)
-    end)
+		shade.drawing:show()
+		table.insert(module._screenShades, shade)
+		shade.timer = timer.doAfter(_duration, function()
+			purgeShade(UUID)
+		end)
+		start_sound:play()
+		return hs.alert.show(str, hs.alert.defaultStyle, screen, _duration)
+	end)
 end
 
 -- Take eye breaks, look away every 20 minutes
-eye_breaks = hs.timer.new(_duration*60, module.shade)
+eye_breaks = hs.timer.new(_duration * 60, module.shade)
 eye_breaks:start()
