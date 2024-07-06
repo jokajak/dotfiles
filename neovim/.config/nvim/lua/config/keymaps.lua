8791bb2885bbda0288eacde3af9662447f04b54c
Josh <jokajak@gmail.com>
2024-07-05 23:07:16 -0400

    chore(neovim): remove aerial
    I found I never use it. Going to try replacing it with trouble

--- neovim/.config/nvim/lua/config/keymaps.lua
+++ neovim/.config/nvim/lua/config/keymaps.lua
@@ -1,6 +1,5 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
--- disable lazygit
-vim.keymap.del("n", "<leader>gg")
-vim.keymap.del("n", "<leader>gG")
+-- disable lazygit log because I use it for linking
+vim.keymap.del("n", "<leader>gl")
--- neovim/.config/nvim/lua/config/lazy.lua
+++ neovim/.config/nvim/lua/config/lazy.lua
@@ -15,7 +15,6 @@ require("lazy").setup({
     -- { import = "lazyvim.plugins.extras.lang.json" },
     -- { import = "lazyvim.plugins.extras.ui.mini-animate" },
     -- import/override with your plugins
-    { import = "lazyvim.plugins.extras.editor.aerial" },
     { import = "lazyvim.plugins.extras.lang.docker" },
     { import = "lazyvim.plugins.extras.lang.markdown" },
     { import = "lazyvim.plugins.extras.lang.python" },
