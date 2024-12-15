933249545feda84eb7716961c6de881eacf94a5c
Josh <jokajak@gmail.com>
2024-12-15 13:19:48 -0500

    feat(wezterm): write out background state

--- wezterm/.config/wezterm/colorschemes/builtin-colorschemes.lua
+++ wezterm/.config/wezterm/colorschemes/builtin-colorschemes.lua
@@ -2,23 +2,51 @@ local M = {}
 
 -- stylua: ignore
 local colorschemes = {
-  { label = "Ayu Dark",          value = "Ayu Dark (Gogh)" },
-  { label = "Tokyo Night Moon",  value = "Tokyo Night Moon"},
-  { label = "Tokyo Night Day",   value = "Tokyo Night Day"},
-  { label = "Catppuccin Mocha",  value = "Catppuccin Mocha"},
-  { label = "Catppuccin Latte",  value = "Catppuccin Latte"},
-  { label = "Oxocarbon Dark",    value = "Oxocarbon Dark (Gogh)" },
-  { label = "Gruvbox Material",  value = "Gruvbox Material (Gogh)" },
-  { label = "Sonokai",           value = "Sonokai (Gogh)" },
-  { label = "Edge Dark",         value = "Edge Dark (base16)" },
-  { label = "Matrix",            value = "matrix" },
+  { label = "Ayu Dark",          value = "Ayu Dark (Gogh)" , background = "dark"},
+  { label = "Tokyo Night Moon",  value = "Tokyo Night Moon", background="dark" },
+  { label = "Tokyo Night Day",   value = "Tokyo Night Day", background="light" },
+  { label = "Catppuccin Mocha",  value = "Catppuccin Mocha", background="dark" },
+  { label = "Catppuccin Latte",  value = "Catppuccin Latte", background="light" },
+  { label = "Oxocarbon Dark",    value = "Oxocarbon Dark (Gogh)" , background="dark" },
+  { label = "Gruvbox Material",  value = "Gruvbox Material (Gogh)" , background="dark" },
+  { label = "Sonokai",           value = "Sonokai (Gogh)" , background="dark" },
+  { label = "Edge Dark",         value = "Edge Dark (base16)" , background="dark" },
+  { label = "Matrix",            value = "matrix" , background="dark" },
 }
+
+local cache_path = os.getenv("HOME") .. "/.local/cache/wezterm/"
+local file_path = cache_path .. "background.lua"
+
+-- Function to write "dark" or "light" based on the active scheme
+local function write_background_type(active_scheme)
+  -- Find the scheme in the list and get its type
+  local background_type = "light"
+  for _, scheme in ipairs(colorschemes) do
+    if scheme.value == active_scheme then
+      background_type = scheme.background
+      break
+    end
+  end
+
+  os.execute("mkdir -p " .. cache_path)
+
+  -- Open the file for writing
+  local file, err = io.open(file_path, "w")
+  if not file then
+    error("Failed to open file: " .. err)
+  end
+
+  -- Write "dark" or "light" to the file
+  file:write(background_type)
+  file:close()
+end
 
 M.init = function()
   return colorschemes
 end
 
 M.activate = function(config, _, value)
+  write_background_type(value)
   config.color_scheme = value
 end
 
