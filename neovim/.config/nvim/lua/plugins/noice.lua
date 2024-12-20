e4b14886e8e759e757b1531919860ac5d7267fd2
Josh <jokajak@gmail.com>
2024-12-19 22:13:44 -0500

    neovim: add noice config back

---
+++ neovim/.config/nvim/lua/plugins/noice.lua
@@ -0,0 +1,9 @@
+local noice = {
+  "folke/noice.nvim",
+  tag = "v4.4.7",
+  keys = {
+    -- disable Ctrl-b so it can be used for specs
+    { "<c-b>", false, mode = { "i", "n", "s" } },
+  },
+}
+return noice
